f024d9a91de44367cc402c3454f5f93a78f30dcf
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-11-16 16:01:44 -0500

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
-	plugins: [require("@tailwindcss/typography"),import("daisyui")],
+	plugins: [require("@tailwindcss/typography"),await import("daisyui")],
 	daisyui: {
 		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: "dark", // name of one of the included themes for dark mode
