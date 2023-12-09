4c93a8eb57200e49da6223bb76b7a63c3a7171bf
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-12-08 22:37:40 -0500

    New astro:assets for Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://turtlecode.tech',
-  integrations: [mdx(), sitemap(), tailwind(), image(
-    {
-      serviceEntryPoint: '@astrojs/image/sharp',
-      cacheDir: "./.cache/image",
-      logLevel: 'debug',
-    }
-  )]
+  integrations: [mdx(), sitemap(), tailwind()]
 });
