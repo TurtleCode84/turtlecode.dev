a3d240a6dcb07cae70fcb4f92f88320f019cd23a
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-12-10 13:43:27 -0500

    fix

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,19 +1,36 @@
-import rss from "@astrojs/rss";
+/*import rss from "@astrojs/rss";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { getCollection } from "astro:content";
 
 export async function get(context) {
-  //const blog = await getCollection("blog");
+  const blog = await getCollection("blog");
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: [],
-    /*items: blog.map((post) => ({
+    items: blog.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
       link: `/blog/${post.slug}/`,
-    })),*/
+    })),
   });
-}
+}*/
+import rss from "@astrojs/rss";
+
+export const get = async () => {
+  // Data fetching, etc.
+
+  return rss({
+    title: "example",
+    description: "Some example description",
+    site: "https://example.pages.dev", //import.meta.env.SITE,
+    items: [
+      {
+        link: "/foo",
+        title: "Foo",
+        pubDate: "2022-01-02",
+      },
+    ],
+  });
+};
