2e3dcfc6a12c5616971e20d0a3f0e1b129fe9126
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-12-10 13:37:31 -0500

    huh

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,16 +4,16 @@ import { getCollection } from "astro:content";
 
 export async function get(context) {
   //const blog = await getCollection("blog");
-  const blog = [{data: {title: "test", pubDate: "Aug 26 1929", description: "Test"}, slug: "#"}]
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: blog.map((post) => ({
+    items: [],
+    /*items: blog.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
       link: `/blog/${post.slug}/`,
-    })),
+    })),*/
   });
 }
