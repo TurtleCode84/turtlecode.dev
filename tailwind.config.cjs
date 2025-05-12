1b5a8aecb3ad3e5923ccda4b794820391401ca73
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2025-05-12 03:00:28 +0000

    Prepare for retroactive upgrade migration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,11 +4,5 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
-	},
-	plugins: [require("@tailwindcss/typography"),require("daisyui")],
-	daisyui: {
-		themes: ["night"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "night", // name of one of the included themes for dark mode
-		logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-	  }
+	}
 }
