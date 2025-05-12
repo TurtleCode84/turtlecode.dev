0309fe0784260bd3694508c8ecff486b83f06b99
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2025-05-12 03:23:45 +0000

    Here goes nothing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://turtlecode.dev',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
